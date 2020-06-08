b45ff8749f4ae6e0cf127b577c4d92f386d37cf2
yuanqiaoyun <1287271332@qq.com>
2020-06-08 09:43:10 +0800

    11

--- knowledge/src/App.js
+++ knowledge/src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import { Layout, Slider } from 'antd';
+const {Header,Content,Footer,Sider} = Layout;
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Layout>
+        <Header className="header">
+          <div>111</div>
+        </Header>
+        <Layout>
+          <Sider>sider</Sider>
+          <Content>Content</Content>
+        </Layout>
+        
+        <Footer>Footer</Footer>
+    </Layout>
     </div>
   );
 }
